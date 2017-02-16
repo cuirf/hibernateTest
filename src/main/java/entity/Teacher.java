c80f40ae5e4c831e7bca6c0b27c03816a5c6e3e2
cuirf <cuirf@sinoauto.com>
2017-02-16 13:49:01 +0800

    第一次修改

---
+++ src/main/java/entity/Teacher.java
@@ -0,0 +1,58 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Teacher implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	
+	public Teacher() {
+		super();
+		
+	}
+	
+	public Teacher(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Teacher other = (Teacher) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
